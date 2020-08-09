b77ee4a90ce44c47f202d3e67e7dd96445e2c5e9
ZenithalHourlyRate <z18ham@gmail.com>
2020-08-09 23:15:38 +0800

    Update readme and give framework

---
+++ nn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+import sys
+import h5py
+import numpy as np
+
+# TODO: open input/output hdf5 files
+input_file = None
+output_file = None
+
+# TODO: access dataset and store them in numpy arrays
+fc1 = np.array(0)
+fc2 = None
+vec = None
+
+# TODO: use numpy related function to realize the computation
+# HINT: you can use np.dot, np.maximum and np.argmax
+imm = None
+relu = None 
+res = None
+argmax = None
+
+# TODO: store the correct data in correct name
+#output_file.create_dataset('name',data=)
